1c8934e5a395673638fd9f63a5ba5cc6688a488f
jslozanod <juansebas2006.jsl@gmail.com>
2025-11-06 18:16:13 -0500

    remove venv from tracking and add gitignore

--- app/main.py
+++ app/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello CI/CD"}
+
+
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"message": f"Hello, {name}!"}
--- tests/test_main.py
+++ tests/test_main.py
@@ -8,3 +8,10 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello CI/CD"}
+
+
+def test_say_hello():
+    response = client.get("/hello/Juan")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, Juan!"}
+
